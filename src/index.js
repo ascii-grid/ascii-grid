3cad90fec150128c47792efa05ba59b73fe62b6b
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-10 14:42:09 -0400

    uninstall to-typed-array and added src/index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+const isAsciiGrid = require("./is-ascii-grid");
+const parseAsciiGridData = require("./parse-ascii-grid-data");
+const parseAsciiGridMetaData = require("./parse-ascii-grid-meta");
+
+module.exports = {
+  isAsciiGrid,
+  parseAsciiGridData,
+  parseAsciiGridMetaData
+};
