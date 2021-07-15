d9888516b340b59de9d4a722949270067fb0e1bc
DanielJDufour <daniel.j.dufour@gmail.com>
2021-07-15 01:33:22 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const isAsciiGrid = require("./is-ascii-grid");
 const parseAsciiGridData = require("./parse-ascii-grid-data");
 const parseAsciiGridMetaData = require("./parse-ascii-grid-meta");
+const calcAsciiGridStats = require("./calc-ascii-grid-stats");
+const iterAsciiGridPoint = require("./iter-ascii-grid-point");
+const forEachAsciiGridPoint = require("./for-each-ascii-grid-point");
 
 module.exports = {
   isAsciiGrid,
   parseAsciiGridData,
-  parseAsciiGridMetaData
+  parseAsciiGridMetaData,
+  calcAsciiGridStats,
+  iterAsciiGridPoint,
+  forEachAsciiGridPoint
 };
